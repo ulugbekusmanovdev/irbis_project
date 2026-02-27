58eda332e8ef2173cf8c02bc94eec8bc4fbb7b18
tagaevr <tagaevr93@gmail.com>
2026-02-27 11:02:31 +0600

    def

--- complect/views.py
+++ complect/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main_page(request):
     return render(request, 'main.html')
+
+def main_page1(request):
+    return render(request, 'main.html')
